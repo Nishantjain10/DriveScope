351a3e4b08188aeff98f7b25b747e4122275ed1a
Nishantjain10 <nishant.jain.775@gmail.com>
2025-08-29 22:25:21 +0530

    fix: remove focus ring from input component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,6 +1,5 @@
-import * as React from "react"
-
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import { cn } from "@/lib/utils";
 
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   return (
@@ -8,13 +7,17 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       type={type}
       data-slot="input"
       className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0 rounded-md border border-gray-200 bg-transparent px-3 py-1 text-base shadow-xs transition-colors outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "aria-invalid:border-red-300",
+        "flex h-9 w-full min-w-0 rounded-md border border-gray-300 bg-white px-3 py-1 text-base md:text-sm",
+        "placeholder:text-gray-400",
+        "focus:border-neutral-300 focus:ring-1 focus:ring-neutral-300 focus:outline-none transition-all",
+        "file:text-foreground selection:bg-primary selection:text-primary-foreground",
+        "disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
+        "aria-invalid:border-red-400",
         className
       )}
       {...props}
     />
-  )
+  );
 }
 
-export { Input }
+export { Input };
