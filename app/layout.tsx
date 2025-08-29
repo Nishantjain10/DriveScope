8419e62d03df4432f828878c1bedfaf111d3d320
Nishantjain10 <nishant.jain.775@gmail.com>
2025-08-29 22:32:24 +0530

    feat: update favicon for dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/stackai.svg" />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
